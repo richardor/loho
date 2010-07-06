c2bc56937b78b17a400200e8fcec0727b4d8f761
dcdcmin@yahoo.com.cn <dcdcmin@yahoo.com.cn@ac14852f-a89d-a3f7-f861-90b7b3b2a1ef>
2010-07-06 06:39:15 +0000

    log的级别也从配置文件里读取

--- opensource/fun_log/src/include/fun_log.h
+++ opensource/fun_log/src/include/fun_log.h
@@ -1,15 +1,27 @@
 #ifndef __FUN_LOG_H__
 #define __FUN_LOG_H__
 
+#ifdef __cplusplus
+ extern "C" {
+#else
+#endif
+
 
 #include<stdarg.h>
 #include<stdio.h>
 #include"fun_log_typedef.h"
 
-
+int fun_log_init();
+int fun_log_uninit();
 int fun_log_set_level(FunLogLevel_t level);
 int fun_log_debug(char *format, ...);
 int fun_log_error(char *format, ...);
+
+
+#ifdef __cplusplus
+}
+#else
+#endif
 
 
 
--- opensource/fun_log/src/test/fun_log_test.c
+++ opensource/fun_log/src/test/fun_log_test.c
@@ -3,7 +3,8 @@
 
 int main()
 {
-   fun_log_set_level(LOG_DEBUG);
+   fun_log_init();
+ //  fun_log_set_level(LOG_DEBUG);
    fun_log_debug("%s 111111111111111\n","debug");
    fun_log_error("%s 222222222222222(%s)\n","error","error");
 
@@ -11,5 +12,7 @@ int main()
    fun_log_set_level(LOG_ERROR);
    fun_log_debug("%s 333333333333333\n","debug");
    fun_log_error("%s 444444444444444(%s)\n","error","error");
+  
+  // fun_log_uninit();
    return 0;	
 }
